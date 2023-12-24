04252fc59f1f7eade87bdefdc70088b1f184bf40
Afranioalves <afranio777alves@gmail.com>
2023-12-25 00:47:28 +0100

    chore: Criar heading component and menu

---
+++ src/components/menu.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import styles from '@/styles/menu.module.css'
+import Image from 'next/image'
+
+const Menu = () => {
+  return (
+    <nav className={styles.container}>
+        <div className={styles.boxLogo}>
+            <Image src='/logo.svg' alt='apa logo' width={70} height={70}/>
+            <p>Associação de<br />programamdores Angolanos</p>
+        </div>
+
+        <ul className={styles.list_link}>
+          <li><a href='#' className={styles.link_selected}>Evento</a></li>
+          <li><a href="#">Agenda</a></li>
+          <li><a href="#">Informações</a></li>
+          <li><a href="#">Patrocinadores</a></li>
+          <li><a href="#">Contacto</a></li>
+        </ul>
+
+        <button className={styles.buttonSupport}>Seja Patrocinador</button>
+    </nav>
+  )
+}
+
+export default Menu
