a35159c1e9e27eb8856baec4afe27bbe7b5f61d8
mraguinaldo <cesaraguinaldo23@gmail.com>
2023-12-26 15:54:38 +0100

    form

--- src/app/sections/event-agenda/header.tsx
+++ src/app/sections/event-agenda/header.tsx
@@ -1,20 +1,23 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { CalendarBlank } from '@phosphor-icons/react/dist/ssr'
 
 import styles from './styles.module.css'
 
 const Header = () => {
+  const { header, focus, special__title, paragraph, calendar__area, span, calendar } = styles
+
   return (
-    <header className={styles.header}>
-      <div className={styles.focus}>
-        <h2 className={styles.special__title}>Agenda do Evento</h2>
-        <p className={styles.paragraph}>Explore nossa agenda repleta de Actividades </p>
+    <header className={header}>
+      <div className={focus}>
+        <h2 className={special__title}>Agenda do Evento</h2>
+        <p className={paragraph}>Explore nossa agenda repleta de Actividades </p>
       </div>
-      <div className={styles.calendar__area}>
-        <span className={styles.span}>
+      <div className={calendar__area}>
+        <span className={span}>
           Data do <br />
           evento por anunciar
         </span>
-        <div className={styles.calendar}>
+        <div className={calendar}>
           <CalendarBlank size={32} color="#fff" />
         </div>
       </div>
--- src/components/footer/about.tsx
+++ src/components/footer/about.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -5,32 +6,24 @@ import { ABOUT } from '../../../utils/data'
 import styles from './style.module.css'
 
 export const About = () => {
+  const { about, nav, link, dev, image, name, stack, content } = styles
+
   return (
-    <div className={styles.about}>
+    <div className={about}>
       {ABOUT.map(({ id, title, children }) => (
         <div key={id} className={styles.children}>
           <h2 className={styles.title}>{title}</h2>
-          <nav className={styles.nav}>
+          <nav className={nav}>
             {children.map((item) => (
-              <Link
-                href={item.href}
-                key={item.id}
-                className={`${styles.link} ${item.stack && styles.dev}`}
-              >
+              <Link href={item.href} key={item.id} className={`${link} ${item.stack && dev}`}>
                 {item.Icon ? (
                   <item.Icon size={32} color="#ffff" />
                 ) : (
-                  <Image
-                    src={item.src}
-                    alt={item.name}
-                    width={48}
-                    height={48}
-                    className={styles.image}
-                  />
+                  <Image src={item.src} alt={item.name} width={48} height={48} className={image} />
                 )}
-                <div className={styles.content}>
-                  <span className={`${styles.name} ${item.stack && styles.dev}`}>{item.name}</span>
-                  <span className={styles.stack}>{item.stack}</span>
+                <div className={content}>
+                  <span className={`${name} ${item.stack && dev}`}>{item.name}</span>
+                  <span className={stack}>{item.stack}</span>
                 </div>
               </Link>
             ))}
