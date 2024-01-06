cde9a66fe03cefaf47dfc43dcb99355065de7c41
Antonio Pedro <tonio.pedro99@gmail.com>
2024-01-06 21:39:11 +0530

    hide agenda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Header from '@/components/header'
 import Headline from '@/components/headline'
 
 import { BenefitsSection } from './sections/benefits'
-import { EventAgendaSection } from './sections/event-agenda'
 import { EventInformationsSection } from './sections/event-information'
 import { OtherEventsSection } from './sections/other-events'
 import { Sponsors } from './sections/sponsors'
@@ -14,15 +13,15 @@ export default function App() {
   return (
     <>
       <Header />
-        <main> 
-          <Headline />
-          <BenefitsSection />
-          {/*   <EventAgendaSection id="schedule" />  */}
-          <EventInformationsSection id="information"/>
-          <OtherEventsSection /> 
-          <Sponsors id="partner"/>
-        </main> 
-      <Footer id="contact"/> 
+      <main>
+        <Headline />
+        <BenefitsSection />
+        {/*   <EventAgendaSection id="schedule" />  */}
+        <EventInformationsSection id="information" />
+        <OtherEventsSection />
+        <Sponsors id="partner" />
+      </main>
+      <Footer id="contact" />
     </>
   )
 }
